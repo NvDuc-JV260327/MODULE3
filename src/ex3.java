06e978f2952361c9d780581d7b0c2ea874a1dd3c
Duc <nvduc9620@gmail.com>
2026-08-16 22:26:48 +0700

    add ex2

---
+++ src/ex3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ex3 {
+
+    public static void main(String[] args) {
+        final double PI = 3.14;
+
+        Scanner scanner = new Scanner(System.in);
+
+        //nhập bán kính
+        System.out.println("Nhập bán kính hình tròn:");
+        double r = scanner.nextDouble();
+
+        //tính diện tích
+        double s = PI * r * r;
+
+        //in ra màn hình
+        System.out.printf("Diện tích: %.2f", s);
+    }
+}
