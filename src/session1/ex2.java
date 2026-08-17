31a23cfec7108ea986d3ae59a50ceb5d933d2e26
Duc <nvduc9620@gmail.com>
2026-08-17 17:08:41 +0700

    update move

--- src/session1/ex2.java
+++ src/session1/ex2.java
@@ -1,3 +1,5 @@
+package session1;
+
 public class ex2 {
     public static void main(String[] args) {
         System.out.printf("Tôi tên là %s, tôi %d tuổi, hiện tại đang học tại PTIT-HCM. Quê ở %s", "Nguyễn Văn A", 22, "Cà Mau");
