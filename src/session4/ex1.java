8bce349246a4a07190c85800757a18351e0408b7
Duc <nvduc9620@gmail.com>
2026-08-20 20:05:31 +0700

    add ss4 ex1

---
+++ src/session4/ex1.java
@@ -0,0 +1,37 @@
+package session4;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class ex1 {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+         System.out.println("Nhập kích thước mảng:");
+         int n = sc.nextInt();
+         int[] arr = new int[n];
+         System.out.println("Nhập các phần tử cho mảng:");
+         for(int i = 0; i < n; i++) {
+             arr[i] = sc.nextInt();
+         }
+         selectionSort(arr);
+         System.out.print("Mảng sau khi đã sắp xếp theo thứ tự giảm dần: ");
+         System.out.println(Arrays.toString(arr));
+         System.out.print("Phần tử lớn nhất trong mảng là: " + arr[0]);
+    }
+
+    //hàm selectionSort
+    public static void selectionSort(int[] arr) {
+         int n = arr.length;
+         for(int i = 0; i < n-1; i++) {
+             int maxIndex = i;
+             for(int j = i+1; j < n; j++) {
+                 if(arr[j] > arr[maxIndex]) {
+                     maxIndex = j;
+                 }
+             }
+             int temp = arr[i];
+             arr[i] = arr[maxIndex];
+             arr[maxIndex] = temp;
+         }
+    }
+}
