e697c2b5fcc623fd5997e2c7aa20deb2c5d8dd1e
Duc <nvduc9620@gmail.com>
2026-09-14 21:41:05 +0700

    update ss11

---
+++ src/session10/ex4/Manage.java
@@ -0,0 +1,7 @@
+package session10.ex4;
+public interface Manage<T> {
+    void add(T item);
+    void update(int index, T item);
+    void delete(int index);
+    void display();
+}
---
+++ src/session11/ex1/Student.java
@@ -0,0 +1,31 @@
+package session11.ex1;
+
+
+public class Student {
+    private String name;
+    private int age;
+    private double averageScore;
+
+    public Student(String name, int age, double averageScore) {
+        this.name = name;
+        this.age = age;
+        this.averageScore = averageScore;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getAverageScore() {
+        return averageScore;
+    }
+
+    @Override
+    public String toString() {
+        return "Tên: " + name + ", Tuổi: " + age + ", Điểm trung bình: " + averageScore;
+    }
+}
